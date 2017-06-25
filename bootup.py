8683ac64456e0c1735c4e6a3413087828f3192da
Piyush <thepiyush@users.noreply.github.com>
2017-06-25 18:34:17 +0530

    Update bootup.py

--- bootup.py
+++ bootup.py
@@ -11,7 +11,14 @@
 #
 
 import os
+import sys
 
-os.system("start /B C:\Python27\pythonw.exe src\breaktime.py") #Run in Background for Window
-#os.system("C:\Python27\python.exe src\breaktime.py") #Run in Foreground for Window (Debugging)
+if(os.name == 'nt'): #For Window
+	os.system("start /B C:\Python27\pythonw.exe src\breaktime.py") #Run in Background
+	#os.system("C:\Python27\python.exe src\breaktime.py") #Run in Foreground (Debugging)
+#elif(os.name == 'mac'): #For MacOS
+#	print("Current OS type: MacOS.")
+else: # Not supported system
+	print("This program is not supported in current OS.")
+	sys.exit(0)
 
