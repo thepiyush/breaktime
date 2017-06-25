79d16749b07b855d373b4d0c62cc0d1b999637b9
Piyush <thepiyush@users.noreply.github.com>
2017-06-25 17:21:13 +0530

    Create bootup.py

---
+++ bootup.py
@@ -0,0 +1,17 @@
+#
+# File name: breaktime.py
+#
+# Author: Piyush
+#
+# Description: Calling Script for breaktime.py (Example)
+# 
+# Chanage log:
+# 06/25/2017 : Initial version. (Piyush)
+#
+#
+
+import os
+
+os.system("start /B C:\Python27\pythonw.exe src\breaktime.py") #Run in Background for Window
+#os.system("C:\Python27\python.exe src\breaktime.py") #Run in Foreground for Window (Debugging)
+
